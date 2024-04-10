115eb4b978b43d9ec5f8083a3744e2b11e9c4e54
Ayan Biswas (DeveloperCom) <147213722+DeveloperCom@users.noreply.github.com>
2024-04-10 09:46:37 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ import express from 'express'
 import fetch from 'node-fetch';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 // Your Telegram bot token
 const BOT_TOKEN = process.env.API_KEY;
